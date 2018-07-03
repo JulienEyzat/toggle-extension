7ef8279ca95948f50c380b65afffb71ab103dd23
Julien Eyzat <julien.eyzat@telecomnancy.net>
2018-07-03 18:20:45 +0200

    add only video and -r

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # toggle-extension
 
-A little command line function to rename all the video files in a folder with their extension or not.
+A little command line function to rename all the only video files in a folder (and optionnaly subdirectories) with their extension or not.
 It works at least for mp4, avi and mkv video formats.
--- toggle-extension.py
+++ toggle-extension.py
@@ -13,10 +13,11 @@ import getopt
 # Variables initialisation
 folder = ''
 op_type = ''
+is_recursive = False
 
 # Get the options
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "arf:")
+    opts, args = getopt.getopt(sys.argv[1:], "adrf:")
 except Exception as e:
     print("You forgot an argument of an option")
     sys.exit(1)
@@ -30,8 +31,10 @@ for o, a in opts:
             folder = a
     elif o == '-a':
         op_type = 'add'
+    elif o == '-d':
+        op_type = 'delete'
     elif o == '-r':
-        op_type = 'remove'
+        is_recursive = True
 
 # Check if all options are ok
 if not folder:
@@ -47,17 +50,27 @@ except Exception as e:
     sys.exit(1)
 
 # Add or remove the extension
-for filename in files:
-    full_filename = folder + '/' + filename
-    type_extension = magic.from_file(full_filename, mime=True)
-    file_type, file_extension = type_extension.split('/')
-    if file_extension == 'x-matroska':
-        file_extension = 'mkv'
-    elif file_extension == 'x-msvideo':
-        file_extension = 'avi'
-    if (op_type == 'add'):
-        new_full_filename = full_filename.rsplit(".", 1)[0]+'.'+file_extension
-    elif (op_type == 'remove'):
-        new_full_filename = full_filename.rsplit(".", 1)[0]
-    os.rename(full_filename, new_full_filename)
-    print(new_full_filename)
+def toggle_extension(files, path):
+    for filename in files:
+        full_filename = path + filename
+        is_dir = os.path.isdir(full_filename)
+        if (is_dir):
+            if (is_recursive):
+                new_files = os.listdir(full_filename)
+                toggle_extension(new_files, path + filename + '/')
+        else:
+            type_extension = magic.from_file(full_filename, mime=True)
+            file_type, file_extension = type_extension.split('/')
+            if file_type == 'video':
+                if file_extension == 'x-matroska':
+                    file_extension = 'mkv'
+                elif file_extension == 'x-msvideo':
+                    file_extension = 'avi'
+                if (op_type == 'add'):
+                    new_full_filename = full_filename.rsplit(".", 1)[0]+'.'+file_extension
+                elif (op_type == 'delete'):
+                    new_full_filename = full_filename.rsplit(".", 1)[0]
+                os.rename(full_filename, new_full_filename)
+                print(new_full_filename)
+
+toggle_extension(files, folder + '/')
